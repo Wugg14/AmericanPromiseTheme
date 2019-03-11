36a0a10b1fce56509c829f630ed98105ab708c33
Mark <mark.steininger@gmail.com>
2019-03-11 14:43:13 -0400

    Small CSS changes, and added php to ensure style.min.css is served unless it is in a debug state

--- lib/functions/load-assets.php
+++ lib/functions/load-assets.php
@@ -10,6 +10,35 @@
  */
 namespace AmericanPromise\AmericanPromiseTheme;
 
+/*
+ * Manage loading of theme styles
+ */
+add_action( 'after_setup_theme', __NAMESPACE__ . '\child_load_stylesheet' );
+
+function child_load_stylesheet() {
+
+	// First, remove the stylesheet
+	remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
+
+	// Enqueue the new stylesheet at a later priority to trump WooCommerce
+	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\child_enqueue_main_stylesheet', 15 );
+}
+
+function child_enqueue_main_stylesheet() {
+	$stylesheet = get_stylesheet_directory() . '/style.css';
+	$cachebust = ( file_exists($stylesheet) ? filemtime($stylesheet) : mt_rand() );
+	$version = defined( 'CHILD_THEME_VERSION' ) && CHILD_THEME_VERSION ? CHILD_THEME_VERSION : PARENT_THEME_VERSION;
+	$handle  = defined( 'CHILD_THEME_NAME' ) && CHILD_THEME_NAME ? sanitize_title_with_dashes( CHILD_THEME_NAME ) : 'child-theme';
+	// Serve minified stylesheet when SCRIPT_DEBUG or WP_DEBUG is false
+	if ( SCRIPT_DEBUG == true || WP_DEBUG == true ) {
+		wp_enqueue_style( $handle, get_stylesheet_directory_uri() . '/style.css', false, $cachebust );
+	}
+	else {
+		wp_enqueue_style( $handle, get_stylesheet_directory_uri() . '/style.min.css', false, $version );
+	}
+
+}
+
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
 /**
  * Enqueue Scripts and Styles.
