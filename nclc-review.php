5460ff3e4f214b7118dbc26d5bdb8183b001e81b
Mark <mark.steininger@gmail.com>
2019-11-06 15:49:20 -0500

    more adjustments to the NCLC review template and css

--- nclc-review.php
+++ nclc-review.php
@@ -14,7 +14,6 @@ remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
 
 // Remove Site Inner Markup for custom homepage
 add_filter( 'genesis_markup_site-inner', '__return_null' );
-add_filter( 'genesis_markup_content-sidebar-wrap_output', '__return_false' );
 add_filter( 'genesis_markup_content', '__return_null' );
 
 remove_action("genesis_loop", "genesis_do_loop");
@@ -32,9 +31,9 @@ function nclc_review(){
     </div>
     <div class="nclc-review__section-1">
         <div class="nclc-review__container">
-            <div class="first two-thirds"><h1>Cause of Our Time: Inviting more young Americans to the movement</h1></div>
+            <div class="first two-thirds"><h1>Cause of Our Time: </h1><h2 style="color: black;">Inviting more young Americans to the movement</h2></div>
             <div class="one-third nclc-review-sub-header__text-block nclc-review-sub-header__text-block--black nclc-review-sub-header__text-block--right"><p>The rising generation of young Americans faces so many issues—among them crippling student loan debt, a broken healthcare system, and the mounting consequences of climate change. Young Americans are ready to tackle the underlying cause of these issues: big money in politics. At the NCLC, we launched the Cause of Our Time Statement of Principle, inviting young Americans to sign on to take a stand against big money in politics. </p></div>
-            <div class="nclc-review__photos-container">
+           <div class="nclc-review__photos-container">
                 <div class="first one-third">
                     <div class="nclc-review__small-photo-container">
                         <img src="https://www.americanpromise.net/wp-content/uploads/2019/11/image1-1.jpeg"/>
@@ -44,7 +43,7 @@ function nclc_review(){
                     </div>
                 </div>
                 <div class="two-thirds">
-                    <div class="nclc_review__collage-container">
+                    <div class="nclc_review__collage-container--right">
                         <div class="nclc_review__image-container"><img width="100%" src="https://www.americanpromise.net/wp-content/uploads/2019/11/Ellen-Greene-Bush.jpg"></div>
                         <div class="nclc_review__card-container">
                             <div class="nclc_review__quote-card">
@@ -65,13 +64,13 @@ function nclc_review(){
             <div class="nclc-review__video-row">
                 <div class="first one-half video-spacer">
                     <div class="video-container">
-                        <iframe width="853" height="480" src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <span class="nclc-review__video-caption"> LOREM IPSUM THIS IS A CAPTION</span>
                 </div>
                 <div class="one-half video-spacer">
                     <div class="video-container">
-                        <iframe width="853" height="480" src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <span class="nclc-review__video-caption"> LOREM IPSUM THIS IS A CAPTION</span>
                 </div>
@@ -82,25 +81,26 @@ function nclc_review(){
 
     <div class="nclc-review__section-2 nclc-review__parallax nclc-review__parallax--teal">
         <div class="nclc-review__container">
-            <div class="first one-third nclc-review-sub-header__text-block nclc-review-sub-header__text-block--left nclc-review-sub-header__text-block--white"><p>Our grassroots movement is successful when citizen leaders across the nation are empowered to win support among their fellow community members, local elected officials, and state officials. Our citizen leaders have been extremely effective in racking up local victories over the course of the past year, and we are ready to double down on these victories in 2020. </p></div>
-            <div class="two-thirds"><h1>Citizen Leadership: Furthering citizen-led action and successes across the nation</h1></div>
+            <div class="first two-thirds"><h1>Citizen Leadership: </h1><h2 class="nclc_review__subheader">Furthering citizen-led action and successes across the nation</h2></div>
+            <div class="one-third nclc-review-sub-header__text-block nclc-review-sub-header__text-block--right nclc-review-sub-header__text-block--white"><p>Our grassroots movement is successful when citizen leaders across the nation are empowered to win support among their fellow community members, local elected officials, and state officials. Our citizen leaders have been extremely effective in racking up local victories over the course of the past year, and we are ready to double down on these victories in 2020. </p></div>
+
             <div class="nclc-review__photos-container">
-                <div class="first one-third">
-                    <div class="nclc-review__small-photo-container">
-                        <img src="https://www.americanpromise.net/wp-content/uploads/2019/11/image1-1.jpeg"/>
-                    </div>
-                    <div class="nclc-review__small-photo-container">
-                        <img src="https://www.americanpromise.net/wp-content/uploads/2019/11/image1-1.jpeg"/>
-                    </div>
-                </div>
-                <div class="two-thirds">
-                    <div class="nclc_review__collage-container">
+                <div class="first two-thirds">
+                    <div class="nclc_review__collage-container--left">
                         <div class="nclc_review__image-container"><img width="100%" src="https://www.americanpromise.net/wp-content/uploads/2019/11/Ellen-Greene-Bush.jpg"></div>
                         <div class="nclc_review__card-container">
                             <div class="nclc_review__quote-card" style="background-color: #5b2b7c;">
                                 <p class="nclc_review__quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus vitae tortor ac tempor. Maecenas egestas odio in nunc aliquam, ac aliquet enim suscipit. Nulla facilisi. Suspendisse consectetur vitae enim nec maximus. </p>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div class="one-third">
+                    <div class="nclc-review__small-photo-container">
+                        <img src="https://www.americanpromise.net/wp-content/uploads/2019/11/image1-1.jpeg"/>
+                    </div>
+                    <div class="nclc-review__small-photo-container">
+                        <img src="https://www.americanpromise.net/wp-content/uploads/2019/11/image1-1.jpeg"/>
                     </div>
                 </div>
             </div>
@@ -115,13 +115,13 @@ function nclc_review(){
             <div class="nclc-review__video-row">
                 <div class="first one-half video-spacer">
                     <div class="video-container">
-                        <iframe width="853" height="480" src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <span class="nclc-review__video-caption"> LOREM IPSUM THIS IS A CAPTION</span>
                 </div>
                 <div class="one-half video-spacer">
                     <div class="video-container">
-                        <iframe width="853" height="480" src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <span class="nclc-review__video-caption"> LOREM IPSUM THIS IS A CAPTION</span>
                 </div>
@@ -133,7 +133,7 @@ function nclc_review(){
 
     <div class="nclc-review__section-3">
         <div class="nclc-review__container">
-            <div class="first two-thirds"><h1>We the People: Building an inclusive movement across political parties, interests, occupations, and generations</h1></div>
+            <div class="first two-thirds"><h1>We the People: </h1><h2 class="nclc_review__subheader">Building an inclusive movement across political parties, interests, occupations, and generations</h2></div>
             <div class="one-third nclc-review-sub-header__text-block nclc-review-sub-header__text-block--white nclc-review-sub-header__text-block--right"><p>Business leaders, veterans, farmers, environmentalists—Americans of all stripes are motivated to end the domination of big money in politics. Our movement invites all Americans to the table, inclusive of all points along the political spectrum, and those from all occupations and walks of life. Learn more about the reasons all types of Americans care about this fundamental issue.</p></div>
             <div class="nclc-review__photos-container">
                 <div class="first one-third">
@@ -145,7 +145,7 @@ function nclc_review(){
                     </div>
                 </div>
                 <div class="two-thirds">
-                    <div class="nclc_review__collage-container">
+                    <div class="nclc_review__collage-container--right">
                         <div class="nclc_review__image-container"><img width="100%" src="https://www.americanpromise.net/wp-content/uploads/2019/11/Ellen-Greene-Bush.jpg"></div>
                         <div class="nclc_review__card-container">
                             <div class="nclc_review__quote-card">
@@ -166,13 +166,13 @@ function nclc_review(){
             <div class="nclc-review__video-row">
                 <div class="first one-half video-spacer">
                     <div class="video-container">
-                        <iframe width="853" height="480" src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <span class="nclc-review__video-caption"> LOREM IPSUM THIS IS A CAPTION</span>
                 </div>
                 <div class="one-half video-spacer">
                     <div class="video-container">
-                        <iframe width="853" height="480" src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <span class="nclc-review__video-caption"> LOREM IPSUM THIS IS A CAPTION</span>
                 </div>
@@ -183,25 +183,25 @@ function nclc_review(){
 
     <div class="nclc-review__section-4 nclc-review__parallax nclc-review__parallax--purple">
         <div class="nclc-review__container">
-            <div class="first one-third nclc-review-sub-header__text-block nclc-review-sub-header__text-block--left nclc-review-sub-header__text-block--white"><p>Building congressional support around the 28th Amendment is a key component of our ratification strategy. Our work to build support in Washington has seen an array of key successes over the past year, and we aim to grow this impact in 2020, with a particular focus on growing collaboration across the political aisle. </p></div>
-            <div class="two-thirds"><h1>Building Congressional Support: Empowering elected officials to take the lead on this cross-partisan issue in Congress </h1></div>
+            <div class="first two-thirds"><h1>Building Congressional Support: </h1><h2 class="nclc_review__subheader">Empowering elected officials to take the lead on this cross-partisan issue in Congress</h2></div>
+            <div class="one-third nclc-review-sub-header__text-block nclc-review-sub-header__text-block--right nclc-review-sub-header__text-block--white"><p>Building congressional support around the 28th Amendment is a key component of our ratification strategy. Our work to build support in Washington has seen an array of key successes over the past year, and we aim to grow this impact in 2020, with a particular focus on growing collaboration across the political aisle. </p></div>
             <div class="nclc-review__photos-container">
-                <div class="first one-third">
-                    <div class="nclc-review__small-photo-container">
-                        <img src="https://www.americanpromise.net/wp-content/uploads/2019/11/image1-1.jpeg"/>
-                    </div>
-                    <div class="nclc-review__small-photo-container">
-                        <img src="https://www.americanpromise.net/wp-content/uploads/2019/11/image1-1.jpeg"/>
-                    </div>
-                </div>
-                <div class="two-thirds">
-                    <div class="nclc_review__collage-container">
+                <div class="first two-thirds">
+                    <div class="nclc_review__collage-container--left">
                         <div class="nclc_review__image-container"><img width="100%" src="https://www.americanpromise.net/wp-content/uploads/2019/11/Ellen-Greene-Bush.jpg"></div>
                         <div class="nclc_review__card-container">
                             <div class="nclc_review__quote-card">
                                 <p class="nclc_review__quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus vitae tortor ac tempor. Maecenas egestas odio in nunc aliquam, ac aliquet enim suscipit. Nulla facilisi. Suspendisse consectetur vitae enim nec maximus. </p>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div class="one-third">
+                    <div class="nclc-review__small-photo-container">
+                        <img src="https://www.americanpromise.net/wp-content/uploads/2019/11/image1-1.jpeg"/>
+                    </div>
+                    <div class="nclc-review__small-photo-container">
+                        <img src="https://www.americanpromise.net/wp-content/uploads/2019/11/image1-1.jpeg"/>
                     </div>
                 </div>
             </div>
@@ -216,13 +216,13 @@ function nclc_review(){
             <div class="nclc-review__video-row">
                 <div class="first one-half video-spacer">
                     <div class="video-container">
-                        <iframe width="853" height="480" src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <span class="nclc-review__video-caption"> LOREM IPSUM THIS IS A CAPTION</span>
                 </div>
                 <div class="one-half video-spacer">
                     <div class="video-container">
-                        <iframe width="853" height="480" src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe src="https://www.youtube.com/embed/E2ej7y1A53Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <span class="nclc-review__video-caption"> LOREM IPSUM THIS IS A CAPTION</span>
                 </div>
