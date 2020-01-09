383711bfc718008bd6ded1cd25582716093f32a8
Wugg14 <mark.steininger@gmail.com>
2020-01-09 11:46:44 -0500

    added masonry.js to the stand with maine testimonials

--- stand-with-maine.php
+++ stand-with-maine.php
@@ -59,42 +59,55 @@ function custom_maine_loop(){
     <div style="background-color:#2f5179;">
         <div class="site-inner">
         <h1 style="color: white; font-size:1.5em;" >What Mainers Are Saying:</h1>
-        <div class="first one-fourth">
-            <figure class="testimonial-card">
-                <blockquote>I am encouraged to see the progress of this proposed Constitutional amendment. We need effective and reasonable regulation of money in our political system to protect the integrity of elections and secure the equal right of all Americans to free speech, representation, and participation in self-government.</blockquote>
-                <div class="council-member">
+            <div class="grid" style="margin:0 auto;">
+                <div class="grid-item">
+                    <figure class="testimonial-card">
+                        <blockquote>I am encouraged to see the progress of this proposed Constitutional amendment. We need effective and reasonable regulation of money in our political system to protect the integrity of elections and secure the equal right of all Americans to free speech, representation, and participation in self-government.</blockquote>
+                        <div class="council-member">
 
-                    <img src="https://www.americanpromise.net/wp-content/uploads/2018/12/snowe.jpg" alt="Olympia Snowe" />
-                    <h5>Olympia Snowe<span class="noautop">Former Senator, (R-Maine)</span><span class="noautop">Lewiston, ME</span></h5>
-                </div></figure>
-        </div>
+                            <img src="https://www.americanpromise.net/wp-content/uploads/2018/12/snowe.jpg" alt="Olympia Snowe" />
+                            <h5>Olympia Snowe<span class="noautop">Former Senator, (R-Maine)</span><span class="noautop">Lewiston, ME</span></h5>
+                        </div></figure>
+                </div>
 
-        <div class="one-fourth">
-            <figure class="testimonial-card">
-                <blockquote>More than 80% of Americans know we need this Constitutional amendment so that all citizens can exercise our free speech rights and participate in self-government on equal terms. Constitutional amendments take persistence and action from Americans who put the country ahead of their political party. Stand With Maine and American Promise are bringing Mainers and all Americans together to make this Constitutional amendment a reality, and all of us will be better for it.</blockquote>
-                <div class="council-member">
+                <div class="grid-item">
+                    <figure class="testimonial-card">
+                        <blockquote>More than 80% of Americans know we need this Constitutional amendment so that all citizens can exercise our free speech rights and participate in self-government on equal terms. Constitutional amendments take persistence and action from Americans who put the country ahead of their political party. Stand With Maine and American Promise are bringing Mainers and all Americans together to make this Constitutional amendment a reality, and all of us will be better for it.</blockquote>
+                        <div class="council-member">
 
-                    <img src="https://www.americanpromise.net/wp-content/uploads/2019/12/tom-allen.jpg" alt="Tom Allen" />
-                    <h5>Tom Allen<span class="noautop">Former Congressman, (D-Maine)</span><span class="noautop">Portland, ME</span></h5>
-                </div></figure>
-        </div>
+                            <img src="https://www.americanpromise.net/wp-content/uploads/2019/12/tom-allen.jpg" alt="Tom Allen" />
+                            <h5>Tom Allen<span class="noautop">Former Congressman, (D-Maine)</span><span class="noautop">Portland, ME</span></h5>
+                        </div></figure>
+                </div>
 
-            <div class="one-fourth">
-                <figure class="testimonial-card">
-                    <blockquote>Maine has been a leader in the fight to get money out of politics, but our democracy is literally drowning in corporate and wealthy donations which is impacting policies across the board. While Americans largely agree on raising the minimum wage, addressing climate change, ensuring universal health care and education, protecting women's rights, protecting our environment, and reducing wealth inequality, uber rich donors are demanding politicians represent them and not the people. It's time to unrig the system once and for all, and we can't do that without rebalancing the money in politics problem.</blockquote>
-                    <div class="council-member">
-                        <img src="https://www.americanpromise.net/wp-content/uploads/2019/12/Headshot-Diane.jpg" alt="Diane Russell" />
-                        <h5>Diane Russell<span class="noautop">Former State Representative, (D-Maine 39th)</span><span class="noautop">Brunswick, ME</span></h5>
-                    </div></figure>
-            </div>
+                <div class="grid-item">
+                    <figure class="testimonial-card">
+                        <blockquote>Maine has been a leader in the fight to get money out of politics, but our democracy is literally drowning in corporate and wealthy donations which is impacting policies across the board. While Americans largely agree on raising the minimum wage, addressing climate change, ensuring universal health care and education, protecting women's rights, protecting our environment, and reducing wealth inequality, uber rich donors are demanding politicians represent them and not the people. It's time to unrig the system once and for all, and we can't do that without rebalancing the money in politics problem.</blockquote>
+                        <div class="council-member">
+                            <img src="https://www.americanpromise.net/wp-content/uploads/2019/12/Headshot-Diane.jpg" alt="Diane Russell" />
+                            <h5>Diane Russell<span class="noautop">Former State Representative, (D-Maine 39th)</span><span class="noautop">Brunswick, ME</span></h5>
+                        </div>
+                    </figure>
+                </div>
 
-        <div class="one-fourth">
-                <figure class="testimonial-card">
-                    <blockquote>Stand with Maine, and its partnership with American Promise nationally, offers a pathway to containing the excessive spending in today’s politics. Acting together, across all parties and independents, we can reverse the divisive impact of unlimited money in political campaigns and restore citizen voice in our democracy.</blockquote>
-                    <div class="council-member">
-                        <img src="https://www.americanpromise.net/wp-content/uploads/2019/12/dick-woodbury.jpg" alt="Dick Woodbury" />
-                        <h5>Dick Woodbury<span class="noautop">Former State Senator, (I-Maine 11th)</span><span class="noautop">Brunswick, ME</span></h5>
-                    </div></figure>
+                <div class="grid-item">
+                    <figure class="testimonial-card">
+                        <blockquote>Stand with Maine, and its partnership with American Promise nationally, offers a pathway to containing the excessive spending in today’s politics. Acting together, across all parties and independents, we can reverse the divisive impact of unlimited money in political campaigns and restore citizen voice in our democracy.</blockquote>
+                        <div class="council-member">
+                            <img src="https://www.americanpromise.net/wp-content/uploads/2019/12/dick-woodbury.jpg" alt="Dick Woodbury" />
+                            <h5>Dick Woodbury<span class="noautop">Former State Senator, (I-Maine 11th)</span><span class="noautop">Brunswick, ME</span></h5>
+                        </div></figure>
+                </div>
+
+                <div class="grid-item">
+                    <figure class="testimonial-card">
+                        <blockquote>I believe that a sustained effort to achieve this amendment will be a legacy Americans, and Mainers in particular, will be proud of in future generations. I want my children to say ‘my mom was an entrepreneur, and she helped pass the 28th Amendment to overturn Citizen’s United.’ I would like them to see that they can innovate, build, achieve their dreams, enjoy living in and working in Maine, and participate in our democracy on equal terms with all other citizens. I urge Maine politicians to lead on this issue, supported by their bi-partisan constituents.</blockquote>
+                        <div class="council-member">
+                            <img src="https://www.americanpromise.net/wp-content/uploads/2020/01/Maranda-Cook.jpg" alt="Marada Cook" />
+                            <h5>Marada Cook<span class="noautop">General Manager, Crown O Maine</span><span class="noautop">North Vassalboro, ME</span></h5>
+                        </div>
+                    </figure>
+                </div>
             </div>
         </div>
     </br>
