bffe6ee37b40123252f6618b396c3b6f4cb4039a
Mark <mark.steininger@gmail.com>
2021-02-09 12:36:56 -0500

    new form assembly zoom registration

--- lib/functions/tracking.php
+++ lib/functions/tracking.php
@@ -11,7 +11,7 @@
 namespace AmericanPromise\AmericanPromiseTheme;
 
 //Tracking for forms
-add_filter( 'gform_field_value_refurl', __NAMESPACE__ . '\populate_referral_url');
+//add_filter( 'gform_field_value_refurl', __NAMESPACE__ . '\populate_referral_url');
 
 add_filter( 'gform_field_value_page_title', __NAMESPACE__ . '\form_title_population' );
 
@@ -62,9 +62,9 @@ function populate_referral_url( $form ){
  */
 function form_title_population() {
     //get the title of the current page
-	$title = get_the_title();
+    $title = get_the_title();
 
-	//return the value of the title
+    //return the value of the title
     return $title;
 }
 
@@ -108,3 +108,26 @@ function check_channel_code_with_js() {
     <div class="channel-code" style="display:none;"><?php echo $_COOKIE['apchannel'] ?></div>
     <?php
 }
+
+/**
+ * Appends the Zoom fields to the url of event pages
+ * This populates the Form Assembly Zoom registration hidden fields
+ *
+ * @since 1.0.12
+ */
+function prefix_filter_news_permalink( $url, $post ) {
+
+    // If the custom_link ACF field is set get it's value
+    $zoom_meeting_id = get_field( 'zoom_meeting_id');
+    $SF_meeting_name = get_field ('sf_campaign_name');
+    $SF_meeting_name = preg_replace('/\s+/', '%20', $SF_meeting_name);
+    $custom_link = $url . '?tfa_122=' . $SF_meeting_name . '&tfa_121=' . $zoom_meeting_id;
+    // If the custom_link is set and the post type is news change the URL to the custom_link value
+    if ($zoom_meeting_id && 'tribe_events' == get_post_type( $post->ID ) ) {
+        $url = $custom_link;
+    }
+    // Return the value of the URL
+    return $url;
+}
+
+add_filter( 'post_type_link', __NAMESPACE__ . '\prefix_filter_news_permalink', 10, 2 );
--- tribe-events/single-event.php
+++ tribe-events/single-event.php
@@ -76,7 +76,159 @@ $event_id = get_the_ID();
 			<?php
 			$removeRsvp = get_field("remove_rsvp");
 			if (!$removeRsvp){
-				echo do_shortcode("[gravityform id=4 title=true description=false ajax=true field_values='your_parameter" .get_the_title(). "']");
+				?>
+                <!-- FORM: HEAD SECTION -->
+
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <meta name="referrer" content="no-referrer-when-downgrade">
+                <script type="text/javascript">
+                    document.addEventListener("DOMContentLoaded", function(){
+                        const FORM_TIME_START = Math.floor((new Date).getTime()/1000);
+                        let formElement = document.getElementById("tfa_0");
+                        if (null === formElement) {
+                            formElement = document.getElementById("0");
+                        }
+                        let appendJsTimerElement = function(){
+                            let formTimeDiff = Math.floor((new Date).getTime()/1000) - FORM_TIME_START;
+                            let cumulatedTimeElement = document.getElementById("tfa_dbCumulatedTime");
+                            if (null !== cumulatedTimeElement) {
+                                let cumulatedTime = parseInt(cumulatedTimeElement.value);
+                                if (null !== cumulatedTime && cumulatedTime > 0) {
+                                    formTimeDiff += cumulatedTime;
+                                }
+                            }
+                            let jsTimeInput = document.createElement("input");
+                            jsTimeInput.setAttribute("type", "hidden");
+                            jsTimeInput.setAttribute("value", formTimeDiff.toString());
+                            jsTimeInput.setAttribute("name", "tfa_dbElapsedJsTime");
+                            jsTimeInput.setAttribute("id", "tfa_dbElapsedJsTime");
+                            jsTimeInput.setAttribute("autocomplete", "off");
+                            if (null !== formElement) {
+                                formElement.appendChild(jsTimeInput);
+                            }
+                        };
+                        if (null !== formElement) {
+                            if(formElement.addEventListener){
+                                formElement.addEventListener('submit', appendJsTimerElement, false);
+                            } else if(formElement.attachEvent){
+                                formElement.attachEvent('onsubmit', appendJsTimerElement);
+                            }
+                        }
+                    });
+                </script>
+
+                <link href="https://www.tfaforms.com/dist/form-builder/5.0.0/wforms-layout.css?v=ef0f21b053cc575f34317757ab64b007f7e59d81" rel="stylesheet" type="text/css" />
+
+                <link href="https://www.tfaforms.com/uploads/themes/theme-73690.css" rel="stylesheet" type="text/css" />
+                <link href="https://www.tfaforms.com/dist/form-builder/5.0.0/wforms-jsonly.css?v=ef0f21b053cc575f34317757ab64b007f7e59d81" rel="alternate stylesheet" title="This stylesheet activated by javascript" type="text/css" />
+                <script type="text/javascript" src="https://www.tfaforms.com/wForms/3.11/js/wforms.js?v=ef0f21b053cc575f34317757ab64b007f7e59d81"></script>
+                <script type="text/javascript">
+                    wFORMS.behaviors.prefill.skip = false;
+                </script>
+                <script type="text/javascript" src="https://www.tfaforms.com/wForms/3.11/js/localization-en_US.js?v=ef0f21b053cc575f34317757ab64b007f7e59d81"></script>
+
+                <!-- FORM: BODY SECTION -->
+                <div class="wFormContainer" >
+                    <div class="wFormHeader"></div>
+                    <style type="text/css"></style><div class=""><div class="wForm" id="4857189-WRPR" dir="ltr">
+                            <div class="codesection" id="code-4857189"></div>
+                            <h3 class="wFormTitle" id="4857189-T">Zoom Registration</h3>
+                            <form method="post" action="https://www.tfaforms.com/responses/processor" class="hintsBelow labelsAbove" id="4857189" role="form">
+                                <div class="oneField field-container-D  labelsRemoved  " id="tfa_122-D"><div class="inputWrapper"><input type="text" id="tfa_122" name="tfa_122" value="Event" default="Event" readonly data-wforms-nooverwrite="disabled" title="Campaign Name" class=""></div></div>
+                                <input type="hidden" id="tfa_121" name="tfa_121" value="" class=""><div id="tfa_12" class="section inline group">
+                                    <div class="oneField field-container-D    " id="tfa_8-D">
+                                        <label id="tfa_8-L" class="label preField reqMark" for="tfa_8">First Name</label><br><div class="inputWrapper"><input type="text" id="tfa_8" name="tfa_8" value="" aria-required="true" title="First Name" class="required"></div>
+                                    </div>
+                                    <div class="oneField field-container-D    " id="tfa_10-D">
+                                        <label id="tfa_10-L" class="label preField reqMark" for="tfa_10">Last Name</label><br><div class="inputWrapper"><input type="text" id="tfa_10" name="tfa_10" value="" aria-required="true" title="Last Name" class="required"></div>
+                                    </div>
+                                </div>
+                                <div id="tfa_118" class="section inline group">
+                                    <div class="oneField field-container-D    " id="tfa_2-D">
+                                        <label id="tfa_2-L" class="label preField reqMark" for="tfa_2">Email</label><br><div class="inputWrapper"><input type="text" id="tfa_2" name="tfa_2" value="" aria-required="true" title="Email" class="validate-email required"></div>
+                                    </div>
+                                    <div class="oneField field-container-D    " id="tfa_117-D">
+                                        <label id="tfa_117-L" class="label preField " for="tfa_117">Confirm email address</label><br><div class="inputWrapper"><input type="text" id="tfa_117" name="tfa_117" value="" title="Confirm email address" class="validate-email"></div>
+                                    </div>
+                                </div>
+                                <div id="tfa_120" class="section inline group">
+                                    <div class="oneField field-container-D    " id="tfa_53-D">
+                                        <label id="tfa_53-L" class="label preField reqMark" for="tfa_53">State</label><br><div class="inputWrapper"><select id="tfa_53" name="tfa_53" title="State" aria-required="true" class="calc-states required"><option value="">Please select...</option>
+                                                <option value="tfa_54" id="tfa_54" class="calcval-AL">AL</option>
+                                                <option value="tfa_55" id="tfa_55" class="calcval-AK">AK</option>
+                                                <option value="tfa_56" id="tfa_56" class="calcval-AZ">AZ</option>
+                                                <option value="tfa_57" id="tfa_57" class="calcval-AR">AR</option>
+                                                <option value="tfa_58" id="tfa_58" class="calcval-CA">CA</option>
+                                                <option value="tfa_59" id="tfa_59" class="calcval-CO">CO</option>
+                                                <option value="tfa_60" id="tfa_60" class="calcval-CT">CT</option>
+                                                <option value="tfa_61" id="tfa_61" class="calcval-DE">DE</option>
+                                                <option value="tfa_62" id="tfa_62" class="calcval-DC">DC</option>
+                                                <option value="tfa_63" id="tfa_63" class="calcval-FL">FL</option>
+                                                <option value="tfa_64" id="tfa_64" class="calcval-GA">GA</option>
+                                                <option value="tfa_65" id="tfa_65" class="calcval-HI">HI</option>
+                                                <option value="tfa_66" id="tfa_66" class="calcval-ID">ID</option>
+                                                <option value="tfa_67" id="tfa_67" class="calcval-IL">IL</option>
+                                                <option value="tfa_68" id="tfa_68" class="calcval-IN">IN</option>
+                                                <option value="tfa_69" id="tfa_69" class="calcval-IA">IA</option>
+                                                <option value="tfa_70" id="tfa_70" class="calcval-KS">KS</option>
+                                                <option value="tfa_71" id="tfa_71" class="calcval-KY">KY</option>
+                                                <option value="tfa_72" id="tfa_72" class="calcval-LA">LA</option>
+                                                <option value="tfa_73" id="tfa_73" class="calcval-ME">ME</option>
+                                                <option value="tfa_74" id="tfa_74" class="calcval-MD">MD</option>
+                                                <option value="tfa_75" id="tfa_75" class="calcval-MA">MA</option>
+                                                <option value="tfa_76" id="tfa_76" class="calcval-MI">MI</option>
+                                                <option value="tfa_77" id="tfa_77" class="calcval-MN">MN</option>
+                                                <option value="tfa_78" id="tfa_78" class="calcval-MS">MS</option>
+                                                <option value="tfa_79" id="tfa_79" class="calcval-MO">MO</option>
+                                                <option value="tfa_80" id="tfa_80" class="calcval-MT">MT</option>
+                                                <option value="tfa_81" id="tfa_81" class="calcval-NE">NE</option>
+                                                <option value="tfa_82" id="tfa_82" class="calcval-NV">NV</option>
+                                                <option value="tfa_83" id="tfa_83" class="calcval-NH">NH</option>
+                                                <option value="tfa_84" id="tfa_84" class="calcval-NJ">NJ</option>
+                                                <option value="tfa_85" id="tfa_85" class="calcval-NM">NM</option>
+                                                <option value="tfa_86" id="tfa_86" class="calcval-NY">NY</option>
+                                                <option value="tfa_87" id="tfa_87" class="calcval-NC">NC</option>
+                                                <option value="tfa_88" id="tfa_88" class="calcval-ND">ND</option>
+                                                <option value="tfa_89" id="tfa_89" class="calcval-OH">OH</option>
+                                                <option value="tfa_90" id="tfa_90" class="calcval-OK">OK</option>
+                                                <option value="tfa_91" id="tfa_91" class="calcval-OR">OR</option>
+                                                <option value="tfa_92" id="tfa_92" class="calcval-PA">PA</option>
+                                                <option value="tfa_93" id="tfa_93" class="calcval-RI">RI</option>
+                                                <option value="tfa_94" id="tfa_94" class="calcval-SC">SC</option>
+                                                <option value="tfa_95" id="tfa_95" class="calcval-SD">SD</option>
+                                                <option value="tfa_96" id="tfa_96" class="calcval-TN">TN</option>
+                                                <option value="tfa_97" id="tfa_97" class="calcval-TX">TX</option>
+                                                <option value="tfa_98" id="tfa_98" class="calcval-UT">UT</option>
+                                                <option value="tfa_99" id="tfa_99" class="calcval-VT">VT</option>
+                                                <option value="tfa_100" id="tfa_100" class="calcval-VA">VA</option>
+                                                <option value="tfa_101" id="tfa_101" class="calcval-WA">WA</option>
+                                                <option value="tfa_102" id="tfa_102" class="calcval-WV">WV</option>
+                                                <option value="tfa_103" id="tfa_103" class="calcval-WI">WI</option>
+                                                <option value="tfa_104" id="tfa_104" class="calcval-WY">WY</option></select></div>
+                                    </div>
+                                    <div class="oneField field-container-D    " id="tfa_4-D">
+                                        <label id="tfa_4-L" class="label preField reqMark" for="tfa_4">Zip Code</label><br><div class="inputWrapper"><input type="text" id="tfa_4" name="tfa_4" value="" aria-required="true" min="00000" max="99999" title="Zip Code" class="validate-integer required"></div>
+                                    </div>
+                                </div>
+                                <div class="oneField field-container-D    " id="tfa_106-D">
+                                    <label id="tfa_106-L" class="label preField " for="tfa_106">As a voter, I identify most strongly as:</label><br><div class="inputWrapper"><select id="tfa_106" name="tfa_106" title="As a voter, I identify most strongly as:" class=""><option value="">Please select...</option>
+                                            <option value="tfa_107" id="tfa_107" class="">Republican</option>
+                                            <option value="tfa_108" id="tfa_108" class="">Liberal</option>
+                                            <option value="tfa_109" id="tfa_109" class="">Conservative</option>
+                                            <option value="tfa_110" id="tfa_110" class="">Independent</option>
+                                            <option value="tfa_111" id="tfa_111" class="">Democrat</option></select></div>
+                                </div>
+                                <div class="oneField field-container-D  labelsRemoved  " id="tfa_113-D" role="group" aria-labelledby="tfa_113-L" data-tfa-labelledby="-L tfa_113-L"><div class="inputWrapper"><span id="tfa_113" class="choices vertical "><span class="oneChoice"><input type="checkbox" value="tfa_114" class="" checked data-default-value="true" id="tfa_114" name="tfa_114" aria-labelledby="tfa_114-L" data-tfa-labelledby="tfa_113-L tfa_114-L"><label class="label postField" id="tfa_114-L" for="tfa_114"><span class="input-checkbox-faux"></span>Email me about progress nationally and in my state</label></span></span></div></div>
+                                <div class="actions" id="4857189-A"><input type="submit" data-label="Register" class="primaryAction" id="submit_button" value="Register"></div>
+                                <div style="clear:both"></div>
+                                <input type="hidden" value="4857189" name="tfa_dbFormId" id="tfa_dbFormId"><input type="hidden" value="" name="tfa_dbResponseId" id="tfa_dbResponseId"><input type="hidden" value="39a04c1bbe381816c52f3b3b1c114d32" name="tfa_dbControl" id="tfa_dbControl"><input type="hidden" value="" name="tfa_dbWorkflowSessionUuid" id="tfa_dbWorkflowSessionUuid"><input type="hidden" value="11" name="tfa_dbVersionId" id="tfa_dbVersionId"><input type="hidden" value="" name="tfa_switchedoff" id="tfa_switchedoff">
+                            </form>
+                        </div></div><div class="wFormFooter"><p class="supportInfo"><a target="new" class="contactInfoLink" href="https://www.tfaforms.com/forms/help/4846892">Contact Information</a><br></p></div>
+                    <p class="supportInfo" >
+                    </p>
+                </div>
+
+                <?php
 			};
 			?>
 		</div> <!-- #post-x -->
